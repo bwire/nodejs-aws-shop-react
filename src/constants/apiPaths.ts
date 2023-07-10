76635f8c7e9a61a5e1e77ba48fa69ff50bbc3b10
barbedwire <e.barbedwire@gmail.com>
2023-07-10 17:12:01 +0200

    add integration changes from task-8

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,10 +1,9 @@
 const API_PATHS = {
-  base: "https://hmovlpdvwa.execute-api.eu-west-1.amazonaws.com/",
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  order: "https://hshxllzqs4.execute-api.eu-west-1.amazonaws.com/api",
   import: "https://yygy70ua14.execute-api.eu-west-1.amazonaws.com/",
   bff: "https://hmovlpdvwa.execute-api.eu-west-1.amazonaws.com/",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "https://hshxllzqs4.execute-api.eu-west-1.amazonaws.com/api",
 };
 
 export default API_PATHS;

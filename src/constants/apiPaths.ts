88a09a1b85c8d8e65f2d0ebacd645340e910a331
barbedwire <e.barbedwire@gmail.com>
2023-07-17 19:47:05 +0200

    alter API path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,5 +1,5 @@
 const API_PATHS = {
-  product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  product: "https://hmovlpdvwa.execute-api.eu-west-1.amazonaws.com/",
   order: "http://bwire-cart-api.eu-west-1.elasticbeanstalk.com/api",
   import: "https://yygy70ua14.execute-api.eu-west-1.amazonaws.com/",
   bff: "https://hmovlpdvwa.execute-api.eu-west-1.amazonaws.com/",
